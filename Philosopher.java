31d6a76f617c21050b5476291832abb604ff22f4
polinasupron <polinasupron@gmail.com>
2025-04-09 15:32:13 +0300

    init new branch

--- Philosopher.java
+++ Philosopher.java
@@ -13,8 +13,7 @@ class Philosopher implements Runnable {
     private final Random random;
     private static final int MAX_EATING = 2000;
     private int eatingCount = 0;
-    private static final Object eatingMonitor = new Object();
-
+    private static final Object eatingMonitor = new Object()
     public Philosopher(int id, Lock leftFork, Lock rightFork) {
         this.id = id;
         this.leftFork = leftFork;
