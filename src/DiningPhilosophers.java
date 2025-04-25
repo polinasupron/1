967b68cb452452818ab8301f3cbe5a14ce58ecf6
polinasupron <polinasupron@gmail.com>
2025-04-25 16:36:27 +0300

    okjuhgtfrw

---
+++ src/DiningPhilosophers.java
@@ -0,0 +1,45 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import java.io.PrintStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DiningPhilosophers {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        int NUM_PHILOSOPHERS = 5;
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Lock[] forks = new ReentrantLock[5];
+        Philosopher[] philosophers = new Philosopher[5];
+
+        for(int i = 0; i < 5; ++i) {
+            forks[i] = new ReentrantLock();
+        }
+
+        for(int i = 0; i < 5; ++i) {
+            Lock leftFork = forks[i];
+                Lock rightFork = forks[(i + 1) % 5];
+            philosophers[i] = new Philosopher(i, leftFork, rightFork);
+            executorService.execute(philosophers[i]);
+        }
+
+        Thread.sleep(50000L);
+        executorService.shutdownNow();
+        executorService.awaitTermination(1L, TimeUnit.SECONDS);
+
+        for(Philosopher philosopher : philosophers) {
+            PrintStream var10000 = System.out;
+            int var10001 = philosopher.getId();
+            var10000.println("Философ " + var10001 + " ел " + philosopher.getEatingCount() + " раз.");
+        }
+
+        System.out.println("Программа завершена.");
+    }
+}
---
+++ src/Philosopher.java
@@ -0,0 +1,80 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+
+class Philosopher implements Runnable {
+    private final int id;
+    private final Lock leftFork;
+    private final Lock rightFork;
+    private final Random random;
+    private static final int MAX_EATING = 2000;
+    private int eatingCount = 0;
+    private static final Object eatingMonitor = new Object();
+
+    public Philosopher(int id, Lock leftFork, Lock rightFork) {
+        this.id = id;
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
+        this.random = new Random();
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getEatingCount() {
+        return this.eatingCount;
+    }
+
+    private void think() throws InterruptedException {
+        System.out.println("Философ " + this.id + " размышляет.");
+        Thread.sleep((long)this.random.nextInt(100));
+    }
+
+    private void eat() throws InterruptedException {
+        System.out.println("Философ " + this.id + " ест.");
+        Thread.sleep((long)this.random.nextInt(100));
+        ++this.eatingCount;
+    }
+
+    public void run() {
+        try {
+            while(!Thread.currentThread().isInterrupted()) {
+                this.think();
+                synchronized(eatingMonitor) {
+                    while(this.eatingCount >= 2000) {
+                        eatingMonitor.wait();
+                    }
+
+                    ++this.eatingCount;
+                }
+
+                this.leftFork.lock();
+
+                try {
+                    if (this.rightFork.tryLock()) {
+                        try {
+                            this.eat();
+                        } finally {
+                            this.rightFork.unlock();
+                        }
+                    }
+                } finally {
+                    this.leftFork.unlock();
+                }
+
+                synchronized(eatingMonitor) {
+                    --this.eatingCount;
+                    eatingMonitor.notifyAll();
+                }
+            }
+        } catch (InterruptedException var20) {
+            Thread.currentThread().interrupt();
+        }
+
+    }
+}
