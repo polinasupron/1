2d542d743c9c328121de7d4ef1737820602c166e
polinasupron <polinasupron@gmail.com>
2025-05-07 18:06:53 +0300

    okjuhgtfr

--- src/DiningPhilosophers.java
+++ src/DiningPhilosophers.java
@@ -15,6 +15,7 @@ public class DiningPhilosophers {
 
 
 
+
     public static void main(String[] args) throws InterruptedException {
         int NUM_PHILOSOPHERS = 5;
         ExecutorService executorService = Executors.newFixedThreadPool(5);
